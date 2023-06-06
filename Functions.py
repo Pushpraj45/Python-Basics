4130f6366451e177fe9d31dcb7b4573d9b7474ed
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-06-07 00:24:42 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -6,7 +6,7 @@ def greetHello(name, ending):
 print("Executing function.....")
 greetHello("Harry", "Thankyou")
 print("done")
-
+# another function with string name 
 # f string
 def letterGenertor(name, date):
     st = f"Hi mam, This is {name} and i will not come to school on {date}"
